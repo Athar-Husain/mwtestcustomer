777757ee4871231aca54435bea78f066c25fa0e2
Athar-Husain <mhatharhusain@gmail.com>
2026-01-09 09:21:10 +0530

    changes in store

---
+++ src/redux/store copy.js
@@ -0,0 +1,41 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import customizationReducer from './features/customization/customizationSlice'; // Import the slice
+
+// import regionReducer from './features/Region/regionSlice';
+// import customerReducer from './features/Customers/customerSlice';
+import adminReducer from './features/Admin/adminSlice';
+import areaReducer from './features/Area/AreaSlice';
+import teamReducer from './features/Team/TeamSlice';
+import customerReducer from './features/Customers/CustomerSlice';
+import connectionReducer from './features/Connection/ConnectionSlice';
+import planReducer from './features/Plan/PlanSlice';
+import ticketReducer from './features/Tickets/TicketSlice';
+
+// added from here
+import notificationReducer from './features/Notifications/NotificationSlice';
+import paymentReducer from './features/Payments/PaymentSlice';
+import upcomingPaymentsReducer from './features/Payments/upcomingPaymentsSlice';
+import billingAddressReducer from './features/Account/billingAddresslice';
+import complaintReducer from './features/Support/ComplaintSlice';
+import leadReducer from './features/Leads/LeadSlice';
+
+export const store = configureStore({
+  reducer: {
+    customization: customizationReducer,
+    admin: adminReducer,
+    area: areaReducer,
+    team: teamReducer,
+    customer: customerReducer,
+    connection: connectionReducer,
+    plan: planReducer,
+    ticket: ticketReducer,
+    // added from here
+    notifications: notificationReducer,
+    payments: paymentReducer,
+    upcomingPayments: upcomingPaymentsReducer,
+    billingAddress: billingAddressReducer,
+    complaints: complaintReducer,
+    lead: leadReducer
+  }
+});
